f363112a260985a6191ba2f6e72f072ede0949bd
aazw <3594744+aazw@users.noreply.github.com>
2025-01-02 10:42:07 +0900

    add comment

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener((urls, sender) => {
 
     // URLを開く
     for (const url of urls) {
-        // 同じウインドウの新しいタブで開く
+        // 同じウインドウの非アクティブな新しいタブで開く
         // https://stackoverflow.com/questions/16503879/chrome-extension-how-to-open-a-link-in-new-tab
         chrome.tabs.create({
             url: url,

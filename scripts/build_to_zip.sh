80029c75b3b54c2900c8bb55653a47de6fbe15f2
aazw <3594744+aazw@users.noreply.github.com>
2025-01-04 20:06:49 +0900

    add script to build zip

---
+++ scripts/build_to_zip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+# How do I get the directory where a Bash script is located from within the script itself?
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}/..
+
+# 出力先ディレクトリ作成
+mkdir -p ./build
+
+# zipファイルに圧縮
+zip ./build/dragopen \
+    background.js \
+    extension.css \
+    extension.js \
+    icons \
+    manifest.json

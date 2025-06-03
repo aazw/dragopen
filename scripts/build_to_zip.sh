7118cf15a5ac43e0d053f26df58cd94d99a7cfbe
aazw <3594744+aazw@users.noreply.github.com>
2025-06-03 20:23:53 +0900

    add dev container and renovate

--- scripts/build_to_zip.sh
+++ scripts/build_to_zip.sh
@@ -4,7 +4,7 @@ set -eu
 
 # How do I get the directory where a Bash script is located from within the script itself?
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd ${SCRIPT_DIR}/..
 
 # 出力先ディレクトリ作成
@@ -12,11 +12,13 @@ mkdir -p ./build
 
 # zipファイルに圧縮
 zip ./build/dragopen \
-    background.js \
-    extension.css \
-    extension.js \
-    icons/icon016.png \
-    icons/icon032.png \
-    icons/icon048.png \
-    icons/icon128.png \
-    manifest.json
+	background.js \
+	extension.css \
+	extension.js \
+	icons/icon016.png \
+	icons/icon032.png \
+	icons/icon048.png \
+	icons/icon128.png \
+	manifest.json
+
+ls -lf build/
